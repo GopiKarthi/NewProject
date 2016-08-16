20d3afdb02ab5e686384572da32cfd2946642ded
GopiKarthi <droidkarthick@gmail.com>
2016-08-16 15:31:07 +0530

    initial commit

--- app/src/main/java/com/drawertab/newproject/MainActivity.java
+++ app/src/main/java/com/drawertab/newproject/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this,"Me",Toast.LENGTH_LONG).show();
+        Toast.makeText(this,"Me gopinath",Toast.LENGTH_LONG).show();
     }
 
     @Override
